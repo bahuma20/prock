0d2c75715e5651fdb861e2d5423215882d3b0cee
Max Bachhuber <max.bachhuber@bahuma.io>
2021-07-21 16:14:57 +0200

    Add aufmass

--- viewer/script.js
+++ viewer/script.js
@@ -69,7 +69,7 @@ window.onload = function () {
       ];
 
       output += data.form.components.map(item => {
-        if (hiddenComponents.indexOf(item.type) !== -1 || item.tags.indexOf('pdfHidden') !== -1) {
+        if (hiddenComponents.indexOf(item.type) !== -1 || item.hasOwnProperty('tags') && item.tags.indexOf('pdfHidden') !== -1) {
           return '';
         }
 
@@ -118,6 +118,10 @@ window.onload = function () {
             </table>`;
 
             break;
+
+          case 'aufmass':
+            value = renderAufmass(value);
+            break;
         }
 
         let inlineTypes = ['datetime', 'number', 'textfield'];
@@ -135,3 +139,129 @@ window.onload = function () {
       document.querySelector('#form').innerHTML = output;
     });
 };
+
+function renderAufmass(value) {
+  console.log(value);
+
+  let rows = ``;
+
+  let positionNr = 0;
+  value.positions.forEach(position => {
+    positionNr++;
+    let positionResult = 0;
+
+    let rowNr = 0;
+    position.rows.forEach(row => {
+      rowNr++;
+      console.log(row);
+      let rowResult = calculateRow(row);
+
+      if (rowResult !== null) {
+        positionResult = positionResult + rowResult;
+      }
+
+      rows += `<tr>
+        <td>${rowNr === 1 ? positionNr : ''}</td>
+        <td class="text-left">${row.label !== null && row.label !== '' ? row.label : ''}</td>
+        <td>${row.type === 'add' && row.count !== null && row.count !== 0 ? row.count : ''}</td>
+        <td>${row.type === 'subtract' && row.count !== null && row.count !== 0 ? row.count : ''}</td>
+        <td class="text-left cell-dimensions">${row.dimensions !== null && row.dimensions !== '' ? formatDimensions(row.dimensions) : ''}</td>
+        <td>${row.type === 'add' && rowResult !== null ? Math.abs(rowResult).toFixed(2) : ''}</td>
+        <td>${row.type === 'subtract' && rowResult !== null ? Math.abs(rowResult).toFixed(2) : ''}</td>
+        <td></td>
+      </tr>`;
+    });
+
+    rows += `<tr>
+      <td></td>
+      <td class="text-left"><strong>Summe</strong></td>
+      <td></td>
+      <td></td>
+      <td class="text-left cell-dimensions"></td>
+      <td></td>
+      <td></td>
+      <td>${positionResult.toFixed(2)}</td>
+    </tr>`;
+
+    rows += `<tr>
+      <td></td>
+      <td class="text-left">&nbsp;</td>
+      <td></td>
+      <td></td>
+      <td class="text-left cell-dimensions"></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>`;
+  });
+
+  let output = `
+  <table class="aufmass">
+    <thead>
+      <tr>
+        <th rowspan="2">Pos.<br>Nr.</th>
+        <th rowspan="2" class="text-left">Bezeichnung</th>
+        <th colspan="2">Stück</th>
+        <th rowspan="2" class="text-left cell-dimensions">Abmessungen</th>
+        <th rowspan="2">Messgehalt</th>
+        <th rowspan="2">Abzug</th>
+        <th rowspan="2">reiner Messgehalt</th>
+      </tr>
+      <tr>
+        <th>+</th>
+        <th>-</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${rows}
+    </tbody>
+  </table>
+  `;
+
+  return output;
+}
+
+
+function formatDimensions(dimensions) {
+  if (!dimensions) {
+    return '';
+  }
+
+  const allowedPunctuation = '+-*/()';
+
+  allowedPunctuation.split('').forEach(item => {
+    // @ts-ignore
+    dimensions = dimensions.replaceAll(item, ' '+item+' ');
+  })
+
+  // @ts-ignore
+  dimensions = dimensions.replaceAll('  ', ' ');
+
+  return dimensions;
+}
+
+
+function calculateRow(row) {
+  if (row.dimensions) {
+    let result;
+    try {
+      result = math.evaluate(row.dimensions);
+    } catch (error) {
+      return null;
+    }
+
+    if (result === Infinity) {
+      return null;
+    }
+
+    result = row.count * result;
+
+    if (row.type === 'subtract') {
+      result = 0 - result;
+    }
+
+    return result;
+  }
+
+  return null;
+}
